36af26bebcd1d4e6a9462042e085560dbb0c2bad
Vishal <91133786+ChamanSahil@users.noreply.github.com>
2023-11-03 17:40:51 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+@app.route('/')
+def build():
+    if response.status_code == 200:
+        labels = response.text.split('\n')
+        print(labels)
+        res = processImage(
+            "https://versatilevats.com/squarehub/glitch/chamanrock145-Lawnmover.jpeg",
+            labels
+        )
+        return res
+    else:
+        print("Error while fetching the labels")
+        return "Error while fetching the labels"
+  
+@app.route('/dreams')
+def dreams():
+    return DREAMS
+  
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
