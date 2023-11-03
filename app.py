8cfe6cdefbad4fb6443c137578e902135eaa3da8
Vishal <91133786+ChamanSahil@users.noreply.github.com>
2023-11-03 17:00:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 from flask import Flask
-vishal = Flask(__name__)
+app = Flask(__name__)
 
 DREAMS = ['India is the best']
 print("Setting up things")
 
-@vishal.route('/')
+@app.route('/')
 def build():
     return "This is a python project"
   
-# @vishal.route('/dreams')
-# def dreams():
-#     return DREAMS
+@app.route('/dreams')
+def dreams():
+    return DREAMS
   
 if __name__ == '__main__':
-    vishal.run()
+    app.run(host='0.0.0.0', port=8080)
