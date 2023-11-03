5e746e57b65384081911dff8d828c0fec8427183
Vishal <91133786+ChamanSahil@users.noreply.github.com>
2023-11-03 16:19:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ vishal = Flask(__name__)
 DREAMS = ['India is the best']
 print("Setting up things")
 
-@vishal.route('/test')
+@vishal.route('/')
 def build():
     return "This is a python project"
   
-@vishal.route('/dreams')
-def dreams():
-    return DREAMS
+# @vishal.route('/dreams')
+# def dreams():
+#     return DREAMS
   
 if __name__ == '__main__':
     vishal.run()
