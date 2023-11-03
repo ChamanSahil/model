74c8f15232a95a1178b17d800b2ae5004a378d9d
Vishal <91133786+ChamanSahil@users.noreply.github.com>
2023-11-03 15:50:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
 from flask import Flask, request, render_template, jsonify
-app = Flask(__name__)
-app.debug = True
+vishal = Flask(__name__)
+vishal.debug = True
 
 DREAMS = ['India is the best']
 print("Setting up things")
 
-@app.route('/test')
+@vishal.route('/test')
 def build():
     print("\nThe Decision Tree is:")
     return "This is a python project"
   
-@app.route('/dreams')
+@vishal.route('/dreams')
 def dreams():
     # Return the list of remembered dreams. 
     return jsonify(DREAMS)
   
 if __name__ == '__main__':
-    app.run()
+    vishal.run()
